cb69a32104877e5d0e82f12db75b92c72808e24a
jatiinyadav <jatin27yadav@gmail.com>
2022-11-10 00:17:26 +0530

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const About = () => {
+  return (
+    <Layout pageTitle="About">
+      <h1>About Page</h1>
+    </Layout>
+  );
+};
+
+export const Head = () => <title>About</title>;
+
+export default About;
